83cdb545f0f000f9953f50d79f44e90c4e7ed9d4
tourifiga <tfmtourifiga@gmail.com>
2024-03-28 14:52:15 +0100

    To test the database

---
+++ database.js
@@ -0,0 +1,72 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+}).promise();
+con.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.error("Connected successfully !!");
+});
+
+async function getstudent() {
+    try {
+        const result = await con.query("SELECT * FROM student");
+        return result[0];
+    } catch (error) {
+        console.error('Error:', error);
+        throw error; // Return null or handle the error as needed
+    } 
+}
+async function getschool(){
+    try{
+        const [result] = await con.query("SELECT * FROM school")
+        return result;
+    }catch(error){
+        console.error("Error:" , error);
+        throw error;
+    }
+}
+async function getschoolByState(state){
+    try{
+        const [result] = await con.query(`
+        SELECT * FROM school WHERE state_sch= ?` , 
+        [state])
+        return result;
+    }catch(error){
+        console.error("Error:" , error);
+        throw error; ;
+    }
+}
+async function insertNewStudent(name , gender , birthdate, email, password){
+    try{
+        const result = await con.query(
+            `INSERT INTO student(name_s, gender_s, birthdate_s, email_s , password_s)
+             VALUES(? , ?, ?, ?, ?)`
+        , [name, gender, birthdate, email, password])
+        return result.insertId;
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+}
+/*const resultOfInsertion = await insertNewStudent('Djalil Palermo' , 'male', '2000-08-31', 'djalilLaClasse@gmail.com', 'may7ebonach');
+console.log(resultOfInsertion);*/
+
+Promise.all([getstudent(),getschool(), getschoolByState('Blida')])
+.then(([result1 ,result2, value])=> {
+    console.log(result1); // Log the result returned by the query
+    console.log(result2);
+    console.log(value);
+    con.end();
+}).catch(error => {
+    console.error('Error:', error);
+    con.end();
+});
